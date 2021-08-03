259961b7f0d7e85e07ef12ae7f003cc68d873721
moon0331 <moon0331@skku.edu>
2021-08-03 23:32:27 +0900

    lv2 done

--- level2/18111_2.py
+++ level2/18111_2.py
@@ -1,34 +1,30 @@
 import sys
 
-def relu(x):
-    return x if x >= 0 else 0
-
 N, M, B = map(int, sys.stdin.readline().split())
 
 land = []
 
-min_land, max_land = 257, -1    # 땅의 최소 높이, 최대 높이
-
 for _ in range(N):
-    row = list(map(int, sys.stdin.readline().split()))
-    min_land, max_land = min(min_land, min(row)), max(max_land, max(row))
-    land.append(row)
+    land.append(list(map(int, sys.stdin.readline().split())))
 
-n_step = []
-for h in range(min_land, max_land+1):
-    #print('=============={}=============='.format(h))
-    n_up, n_down = 0, 0
-    for row in land:
-        #print(sum(relu(h-x) for x in row), "needed to up\t", h, row)
-        n_up += sum(relu(h-x) for x in row)
-        #print(sum(relu(x-h) for x in row), "needed to down", h, row)
-        n_down += sum(relu(x-h) for x in row)
-    time = 2 * n_down + n_up
-    if B >= n_up:
-        n_step.append((h, n_up, n_down, time))
+ans_height = -1
+ans_time = float('inf') #sys.maxint
+for height in range(256+1):
+    h_down = h_up = 0
+    for i in range(N):
+        for j in range(M):
+            if land[i][j] > height:
+                h_down += land[i][j] - height
+            else:
+                h_up += height - land[i][j]
+    if h_up - h_down > B:
+        continue
+    h_time = 2 * h_down + h_up
+    if h_time <= ans_time:
+        ans_height = height
+        ans_time = h_time
 
-n_step.sort(key=lambda x:(x[-1], -x[0])) # time 오름차순, h 내림차순
+print(ans_time, ans_height)
 
-ans = n_step[0]
-
-print(ans[-1], ans[0])
+# 올라가는 것과 내려가는 것이 가중치가 다르기 때문에
+# 무조건 옵티멀이 최소 최대 사이가 아닐 수 있다
--- level2/1874_stack.py
+++ level2/1874_stack.py
@@ -1,10 +1,24 @@
-from abc import abstractproperty
 import sys
 
 n = int(sys.stdin.readline())
 
-arr = []
+cnt = 0
+stack = []
+result=''
 
 for _ in range(n):
-    arr.append(int(sys.stdin.readline()))
+    x = int(sys.stdin.readline())
+    while cnt < x:
+        cnt += 1
+        stack.append(cnt)
+        result += '+'
+    
+    if stack[-1] == x:
+        stack.pop()
+        result += '-'
+    else:
+        print('NO')
+        exit(0)
 
+for c in result:
+    print(c)
